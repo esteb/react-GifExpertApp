17079ea8dfa9ff2ca2c371fcf9fb759602cf86c2
Esteban <esteban.gutm@gmail.com>
2022-04-08 09:20:34 -0400

    se elimina setTime de 3 segundos

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -12,12 +12,20 @@ export const useFetchGifs = ( category ) => {
         getGifs( category )
             .then( imgs => {
 
+                /*
                 setTimeout( () => {
                     setState({
                         data: imgs,
                         loading: false
                     });
                 }, 3000 );
+                */
+
+                setState({
+                    data: imgs,
+                    loading: false
+                });
+                
             });                        
     }, [ category ])
 
